3cc7b4f63adee063ccc91cc3e2760f3628defe01
Holonium <holonium2.0@gmail.com>
2021-03-24 10:23:41 -0700

    Began implementation version of the core

---
+++ implementation/HoloRiscV.v
@@ -0,0 +1,15 @@
+/*
+
+This version of the core is designed for implementation on a chip. Currently, only the RV32I ISA is being implemented.
+The exception to this is the EBREAK, ECALL, and FENCE instructions.
+The pipeline is composed of 5 stages, none of which currently run in parallel.
+
+*/
+
+`default_nettype none
+
+module HoloRiscV (
+
+	);
+	
+endmodule
